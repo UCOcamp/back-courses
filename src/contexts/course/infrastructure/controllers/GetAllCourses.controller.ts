4da3411e4631db8845c2ca28954a862b6e62373a
leivaa21 <adrianleiva212@gmail.com>
2022-05-17 00:10:56 +0200

    feat: GetAllCourses Endpoint working

---
+++ src/contexts/course/infrastructure/controllers/GetAllCourses.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, NotFoundException } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import GetAllCoursesQuery from '../../application/useCases/GetAllCourses/queries/GetAllCourses.handler';
+import GetAllCoursesRequest from '../../application/useCases/GetAllCourses/requests/GetAllCourses.request';
+import {
+  GetAllCoursesResponse,
+  GetAllCoursesResponseJSON,
+} from '../../application/useCases/GetAllCourses/responses/GetAllCourses.response';
+
+@Controller('courses')
+@ApiTags('courses')
+class GetAllCoursesController {
+  constructor(private readonly queryBus: QueryBus) {}
+  @Get('/')
+  @ApiOperation({
+    summary: 'Get all the courses saved',
+  })
+  @ApiOkResponse({
+    status: 200,
+    description: 'Courses returned succesfully',
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Courses not found!',
+  })
+  async getAllCourses(): Promise<GetAllCoursesResponseJSON> {
+    const courses = await this.queryBus.execute<
+      GetAllCoursesQuery,
+      GetAllCoursesResponse
+    >(new GetAllCoursesQuery(new GetAllCoursesRequest()));
+    if (courses.length === 0) throw new NotFoundException('Courses not found!');
+    return courses.map((course) => course.json);
+  }
+}
+
+export default GetAllCoursesController;
--- src/contexts/course/infrastructure/controllers/_index.ts
+++ src/contexts/course/infrastructure/controllers/_index.ts
@@ -1,3 +1,7 @@
 import CreateCourseController from './CreateCourse.controller';
+import GetAllCoursesController from './GetAllCourses.controller';
 
-export const CourseControllers = [CreateCourseController];
+export const CourseControllers = [
+  CreateCourseController,
+  GetAllCoursesController,
+];
