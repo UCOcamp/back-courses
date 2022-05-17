10a255644c60afa21874ae7137bdff3cee750bad
leivaa21 <adrianleiva212@gmail.com>
2022-05-17 11:25:21 +0200

    fix: updated usecases index

--- src/contexts/course/application/useCases/_index.ts
+++ src/contexts/course/application/useCases/_index.ts
@@ -1,7 +1,15 @@
+import CreateContentBlockHandler from './CreateContentBlock/commands/CreateContentBlock.handler';
+import ContentBlockWasCreatedHandler from './CreateContentBlock/events/ContentBlockWasCreated.handler';
 import CreateCourseHandler from './CreateCourse/commands/CreateCourse.handler';
 import CourseWasCreatedHandler from './CreateCourse/events/CourseWasCreated.handler';
 import GetAllCoursesHandler from './GetAllCourses/queries/GetAllCourses.query';
 
-export const CourseCommandHandlers = [CreateCourseHandler];
+export const CourseCommandHandlers = [
+  CreateCourseHandler,
+  CreateContentBlockHandler,
+];
 export const CourseQueryHandlers = [GetAllCoursesHandler];
-export const CourseEventHandlers = [CourseWasCreatedHandler];
+export const CourseEventHandlers = [
+  CourseWasCreatedHandler,
+  ContentBlockWasCreatedHandler,
+];
