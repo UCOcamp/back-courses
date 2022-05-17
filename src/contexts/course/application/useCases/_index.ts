ada58890317e1d56e1f3a6afff9435e515437ce4
leivaa21 <adrianleiva212@gmail.com>
2022-05-17 12:37:26 +0200

    feat: CreateSubscription use case

--- src/contexts/course/application/useCases/_index.ts
+++ src/contexts/course/application/useCases/_index.ts
@@ -2,14 +2,18 @@ import CreateContentBlockHandler from './CreateContentBlock/commands/CreateConte
 import ContentBlockWasCreatedHandler from './CreateContentBlock/events/ContentBlockWasCreated.handler';
 import CreateCourseHandler from './CreateCourse/commands/CreateCourse.handler';
 import CourseWasCreatedHandler from './CreateCourse/events/CourseWasCreated.handler';
+import CreateSubscriptionHandler from './CreateSubscription/commands/CreateSubscription.handler';
+import SubscriptionWasCreatedHandler from './CreateSubscription/events/SubscriptionWasCreated.handler';
 import GetAllCoursesHandler from './GetAllCourses/queries/GetAllCourses.query';
 
 export const CourseCommandHandlers = [
   CreateCourseHandler,
   CreateContentBlockHandler,
+  CreateSubscriptionHandler,
 ];
 export const CourseQueryHandlers = [GetAllCoursesHandler];
 export const CourseEventHandlers = [
   CourseWasCreatedHandler,
   ContentBlockWasCreatedHandler,
+  SubscriptionWasCreatedHandler,
 ];
